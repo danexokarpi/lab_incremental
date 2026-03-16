28a92d5133242cd6275bb597350f26e72560e1c0
GuimaD <v4duarte@gmail.com>
2026-03-16 20:31:53 -0300

    implementação de ArrayList na PilhaDeDescarte

--- src/PilhaDeDescarte.java
+++ src/PilhaDeDescarte.java
@@ -1,18 +1,30 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class PilhaDeDescarte {
-    private Deque<Carta> pilha = new ArrayDeque<Carta>();
+    private ArrayList<Carta> pilha = new ArrayList<Carta>();
+    private Random random = new Random();
 
-    public PilhaDeDescarte() {
-        //
+    public PilhaDeDescarte(Carta[] cartas) {
+        for (int i = 0; i < cartas.length; i++) {
+            this.pilha.add(cartas[i]);
+        }
     }
 
     public Carta pop() {
-        return pilha.pollFirst();
+        Carta primeiraCarta = pilha.get(0);
+        pilha.remove(0);
+        return primeiraCarta;
+    }
+
+    public Carta popRandom() {
+        int randomIndex = random.nextInt();
+        Carta cartaEscolhida = pilha.get(randomIndex);
+        pilha.remove(randomIndex);
+        return cartaEscolhida;
     }
 
     public void push(Carta carta) {
-        pilha.push(carta);
+        pilha.add(carta);
     }
 }
