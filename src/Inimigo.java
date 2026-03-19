82ae7aa82de676064f34ec1086d68edc3c4c2d78
danexokarpi <danilohbkarpiuck@gmail.com>
2026-03-19 15:15:09 -0300

    Mudannnnnças para a adição de possibilidades de ação em cada turno

--- src/Inimigo.java
+++ src/Inimigo.java
@@ -1,10 +1,36 @@
-public class Inimigo extends Entidade {
-    private int dano;
+import java.util.Random;
 
-    public Inimigo(String nome, int vidaMaxima, int escudo, int dano) {
+public class Inimigo extends Entidade {
+    private static Random random = new Random();
+    private int dano;
+    private int cura;
+    private int escudoRecebido;
+    private char[] listaDeAcoes;
+    private char proximaAcao;
+
+    public Inimigo(String nome, int vidaMaxima, int escudo, int dano, int cura, char[] listaDeAcoes) {
         super(nome, vidaMaxima, escudo);
         this.dano = dano;
+        this.listaDeAcoes = listaDeAcoes; 
+        this.cura = cura;
+        this.proximaAcao = listaDeAcoes[random.nextInt(listaDeAcoes.length)];
     }
+
+    public void agir(Tabuleiro tabuleiro) {
+        switch (proximaAcao) {
+            case 'A':
+                tabuleiro.getHeroi().receberDano(dano);
+                break;
+            case 'C':
+                this.curar(cura);
+                break;
+            case 'E':
+                this.receberEscudo(escudoRecebido);
+                break;
+        }
+    }
+
+
 
     public int getDano() {
         return this.dano;
