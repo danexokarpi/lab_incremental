5a0ac33ecf51b6450a7f7b0e30c829856baa8c79
danexokarpi <danilohbkarpiuck@gmail.com>
2026-03-29 14:39:00 -0300

    Adicionado o Enum de eventos possíveis até agora

--- app/src/main/java/org/jogo/Efeito.java
+++ app/src/main/java/org/jogo/Efeito.java
@@ -7,13 +7,30 @@ public abstract class Efeito {
     private int acumulos;
     private boolean ativo;
 
-    protected Efeito (String nome, String tipoDeEfeito, Entidade dono, int acumulos){
+    protected Efeito (String nome, String tipoDeEfeito, int acumulos){
         this.nome = nome;
         this.tipoDeEfeito = tipoDeEfeito;
-        this.dono = dono;
+        this.dono = null;
         this.acumulos = acumulos;
         this.ativo = acumulos > 0;
     }
+    
+    /* Os eventos são so seguintes
+    1-Fim do round
+    2-Fim do turno do Jogador
+    3-Fim do turno de uma Entidade
+    4-Ataque de Entidade
+    5-Entidade recebe dano
+    */
+    public enum gatilhos{
+        FimDoRound,
+        FimDoTurnoDoJogador,
+        FimDoTurnoDeUmaEntidade,
+        AtaqueDeEntidade,
+        EntidadeRecebeDano
+
+    }
+
 
     public String getString() {
         return "[ " + nome + "(" + acumulos + "x) ]";
@@ -30,9 +47,17 @@ public abstract class Efeito {
     public String getTipoDeEfeito(){
         return this.tipoDeEfeito;
     }
+
+    public void setDono(Entidade dono){
+        this.dono = dono;
+    }
  
     public void somaAcumulos(int acumulosRecebidos) {
         this.acumulos += acumulosRecebidos;
+    }
+
+    public isAtivo(){
+        return ativo;
     }
 
     public void subtrairAcumulo(){
@@ -43,12 +68,6 @@ public abstract class Efeito {
         }
         }
     }
-    /* Os eventos são so seguintes
-    1-Fim do round
-    2-Fim do turno do Jogador
-    3-Fim do turno de uma Entidade
-    4-Ataque de Entidade
-    5-Entidade recebe dano
-    */
+    
     public abstract void receberNotificacao(int evento);
 }
