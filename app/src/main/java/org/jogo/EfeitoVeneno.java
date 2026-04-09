9b93e40a520bc5f97b8cfc1ebad60389a394fe42
danexokarpi <danilohbkarpiuck@gmail.com>
2026-04-09 14:45:33 -0300

    Mudanças de documentação

--- app/src/main/java/org/jogo/EfeitoVeneno.java
+++ app/src/main/java/org/jogo/EfeitoVeneno.java
@@ -1,4 +1,8 @@
 package org.jogo;
+
+/**
+ * O efeito de veneno, em todo fim de turno, da dano no seu recepetor de acordo com a quantidade de stacks desse efeito que ele possui, e a cada turno uma stack é consumida.
+*/
 public class EfeitoVeneno extends Efeito {
     public EfeitoVeneno (int acumulos){
         super("Veneno", "Debuff", acumulos, "Veneno causa dano equivalente ao número de stacks e perde um stack todo fim de round");
