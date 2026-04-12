c2897237066b742bc678fd5b00fb9053cf304226
GuimaD <v4duarte@gmail.com>
2026-04-12 13:21:45 -0300

    Classe MenuPrincipal (com título do jogo temporário)

--- app/src/main/java/org/jogo/App.java
+++ app/src/main/java/org/jogo/App.java
@@ -22,16 +22,21 @@ public class App {
                 "Crie um campo magnético em volta de si; a única fraqueza do sabre de fótons", 1, 2);
         CartaEfeito stimPack = new CartaEfeito("Stimpack",
                 "Uma grande dose de seilá o que direto nas suas veias, vai curar quase qualquer ferida," +
-                "menos as psicológicas", 2, new FabricaDeEfeito("regeneracao", 3), "Unico");
-        CartaEfeito frascoDeVeneno = new CartaEfeito("Frasco de Veneno", "Um pequeno fraco com veneno concentrado, lance apenas em inimigos!",
-                2 , new FabricaDeEfeito("veneno", 3), "Unico");
+                        "menos as psicológicas",
+                2, new FabricaDeEfeito("regeneracao", 3), "Unico");
+        CartaEfeito frascoDeVeneno = new CartaEfeito("Frasco de Veneno",
+                "Um pequeno fraco com veneno concentrado, lance apenas em inimigos!",
+                2, new FabricaDeEfeito("veneno", 3), "Unico");
         Carta[] listaBaralho = { laserCinético, sabreDeFotons,
                 escudoEletromagneticoGrande, escudoEletromagneticoPequeno,
-                stimPack, frascoDeVeneno};
+                stimPack, frascoDeVeneno };
         return new ArrayList<Carta>(Arrays.asList(listaBaralho));
     }
 
     public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.titulo();
+
         Heroi heroi = new Heroi("Capitão Cabra", 15, 0);
         Inimigo inimigo1 = new Inimigo("Escorpião Gigante", 20, 0, 4, 0, 2, new FabricaDeEfeito("veneno", 3),
                 new char[] { 'A', 'E', 'U' });
@@ -44,8 +49,8 @@ public class App {
 
         ArrayList<Carta> baralho = criaBaralho();
 
-        Tabuleiro tabuleiro = new Tabuleiro(heroi, inimigos, baralho, energiaMaxima, 2);
-        tabuleiro.novaBatalha();
+        Batalha batalha = new Batalha(heroi, inimigos, baralho, energiaMaxima, 2);
+        batalha.novaBatalha();
     }
 
 }
