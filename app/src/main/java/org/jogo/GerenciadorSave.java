59f54233cc93514e5ed852fdec48933234da5a43
danexokarpi <danilohbkarpiuck@gmail.com>
2026-04-13 19:38:49 -0300

    Adição de opções de salvamento

---
+++ app/src/main/java/org/jogo/GerenciadorSave.java
@@ -0,0 +1,11 @@
+package org.jogo;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GerenciadorSave {
+    
+}
