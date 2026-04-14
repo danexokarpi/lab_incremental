84624e516b2aaf3f01fbffd9c9ec9c64b19a9d66
GuimaD <v4duarte@gmail.com>
2026-04-13 22:39:48 -0300

    adição das classes Mapa.java e NoMapa.java (ainda em andamento)

---
+++ app/src/main/java/org/jogo/Mapa.java
@@ -0,0 +1,35 @@
+package org.jogo;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Mapa {
+
+    private ArrayList<NoMapa> listaDeNos;
+    private ArrayList<Batalha> listaDeBatalhas;
+
+    public Mapa(String arquivoDoMapa) {
+        InputStream esqueletoDoMapa = MenuPrincipal.class.getResourceAsStream("/esqueletoDoMapa.txt");
+        if (esqueletoDoMapa == null) {
+            throw new IllegalArgumentException("Nenhum arquivo com nome correspondente");
+        }
+        Scanner scannerMapa = new Scanner(esqueletoDoMapa);
+        int index = 0;
+        while (scannerMapa.hasNextLine()) {
+            NoMapa noAtual = new NoMapa(index);
+            listaDeNos.add(noAtual);
+            String[] filhosDoNo = scannerMapa.nextLine().split(" ");
+            for (String filho : filhosDoNo) {
+                noAtual.addFilho(Integer.parseInt(filho));
+            }
+            index++;
+        }
+        scannerMapa.close();
+    }
+
+    public Batalha getBatalha(int idNo) {
+        return listaDeBatalhas.get(idNo);
+    }
+
+}
